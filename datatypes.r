bf99d13cf13bc0c940b5ec5c58a488507babf299
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-08-01 21:23:35 +0530

    Update datatypes.r

--- datatypes.r
+++ datatypes.r
@@ -1,3 +1,7 @@
 a <- 10
 b <- 20
 paste("sum of a and b is",a+b)
+paste("difference of a and b is",a-b)
+paste("multiplication of a and b is",a*b)
+paste("division of a and b is",a/b)
+paste("modulus of a and b is",a%%b)
