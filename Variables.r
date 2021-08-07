9350fefcbafc5f83cde9ba1704bafb5ef60a2d85
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-08-07 18:35:09 +0530

    Update Variables.r

--- Variables.r
+++ Variables.r
@@ -5,3 +5,27 @@ paste("difference of a and b is",a-b)
 paste("multiplication of a and b is",a*b)
 paste("division of a and b is",a/b)
 paste("modulus of a and b is",a%%b)
+
+c <- d <- e <- "python is king"
+
+print(c)
+
+print(d)
+
+print(e)
+
+
+
+
+# OUTPUT
+
+#[1] "sum of a and b is 30"
+#[1] "difference of a and b is -10"
+#[1] "multiplication of a and b is 200"
+#[1] "division of a and b is 0.5"
+#[1] "modulus of a and b is 10"
+#[1] "python is king"
+#[1] "python is king"
+#[1] "python is king"
+#
+#[Program exited with exit code 0]
