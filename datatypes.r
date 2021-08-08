07c39d63865cd6b8ec7a42949f86ef7f4936d63e
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-08-08 21:24:42 +0530

    Update datatypes.r

--- datatypes.r
+++ datatypes.r
@@ -1,1 +1,23 @@
+# numeric type
+a <- 2.5
+a2 <- 25 
+
+# character type / string
+b <- "machine_learning"
+
+# integer type
+c <- 200L
+
+# complex
+d <- 20 + 30i
+
+# bool
+e <- TRUE
+
+paste(a,class(a))
+paste(a2,class(a2))
+paste(b,class(b))
+paste(c,class(c))
+paste(d,class(d))
+paste(e,class(e))
 
