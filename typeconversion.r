205d86069d58bd38cbdc9fe31a28361ad2435ed6
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-08-08 21:36:33 +0530

    Update typeconversion.r

--- typeconversion.r
+++ typeconversion.r
@@ -1,1 +1,22 @@
+# typeconvert int to string
+a <- 123
+b <- as.character(a)
+
+paste(b,class(b))
+
+# typeconvert string to numeric
+a <- "123"
+b <- as.numeric(a)
+paste(b,class(b))
+
+
+# typeconvert string to integer
+a <- "123.25"
+b <- as.integer(a)
+paste(b,class(b))
+
+
+
+
+
 
