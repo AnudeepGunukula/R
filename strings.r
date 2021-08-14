4eb113f09e3e79a1a27bf0199ce9cfe774030d12
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-08-14 22:07:49 +0530

    Update strings.r

--- strings.r
+++ strings.r
@@ -1,1 +1,33 @@
+st <- 
+'hello
+ mars'
+cat(st)  #prints line breaks as in the code
+
+
+print(st) # prints in a single line not caring about breaks
+
+
+print(nchar(st))  # len() in python
+
+
+print(grepl('Hel',st))   # similar to in
+
+
+print(grepl('hel',st))
+
+str1 = 'hello'
+str2 = 'world'
+paste(str1,str2)
+
+str3 = "python \"dev\"eloper". # use '\' as escape
+cat(str3)
+
+
+
+
+# OUTPUT
+
+
+
+
 
