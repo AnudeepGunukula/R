978d4f6195bfdbe9dab3305d9644aa3cee394ebb
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-08-14 22:14:14 +0530

    Update strings.r

--- strings.r
+++ strings.r
@@ -19,13 +19,11 @@ str1 = 'hello'
 str2 = 'world'
 paste(str1,str2)
 
-str3 = "python \"dev\"eloper". # use '\' as escape
+str3 = "python \"dev\"eloper"  # use '\' as escape
 cat(str3)
 
 
-
-
-# OUTPUT
+#OUTPUT
 
 #hello
 # mars[1] "hello\n mars"
@@ -33,11 +31,6 @@ cat(str3)
 #[1] FALSE
 #[1] TRUE
 #[1] "hello world"
-#Error: unexpected symbol in "str3 = "python \"dev\"eloper"."
-#Execution halted
-#
-#[Program exited with exit code 1]
-
-
-
+#python "dev"eloper
+#[Program exited with exit code 0]
 
