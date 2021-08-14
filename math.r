af23e46c7b284d955336258934569659d9446972
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-08-14 21:41:17 +0530

    Update math.r

--- math.r
+++ math.r
@@ -1,1 +1,20 @@
+x <- min(1,2,3)
+paste(x,class(x))
+
+y <- max(1,2,3)
+paste(y,class(y))
+
+z <- sqrt(25)
+print(z)
+print(class(z))
+
+w <- abs(-5.6)
+paste(w,class(w))
+
+u <- ceiling (4.5)
+paste(u,class(u))
+
+v <- floor(6.7)
+paste(v,class(v))
+
 
