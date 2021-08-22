8a8ba4320e4cde551d7861b3b4b76b9a334764d8
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-08-22 21:09:29 +0530

    Update Conditional.r

--- Conditional.r
+++ Conditional.r
@@ -1,3 +1,4 @@
+
 a <- 2
 b <- 4
 
@@ -11,3 +12,46 @@ if(a<b)
 {
     print('a is equal to b')
 }
+
+# if braces not given as same syntax error will occur
+
+c <- 12
+
+if(c>10)
+{
+  print('c is greater than 10')
+  if(c>20)
+  {
+      print('and c is greater than 20')
+  }else
+  {
+      print('and c is less than 20')
+  }
+}else
+{
+    print('c is less than 10')
+}
+
+
+d <- 10
+e <- 20
+f <- 30
+if(d<e & e<f){
+    print(' both conditions are true')
+}
+
+if(d>e | e<f){
+    print('atleast one of the conditions is true')
+}
+
+
+# OUTPUT: 
+
+#[1] "a is less than b"
+#[1] "c is greater than 10"
+#[1] "and c is less than 20"
+#[1] " both conditions are true"
+#[1] "atleast one of the conditions is true"
+#
+#[Program exited with exit code 0]
+
