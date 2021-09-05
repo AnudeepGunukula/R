ee17cf07c6f1a0f0b10c387b5d763ebfa734c3e7
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-09-05 14:13:09 +0530

    Update read_input.r

--- read_input.r
+++ read_input.r
@@ -2,3 +2,11 @@
 #most online compilers are work on non interactive mode so 
 #taking input using readline() and scan() will not work 
 #readLines() is the best option
+
+#reading all input at once as a character vector
+input1 <- readLines("stdin")
+
+for(i in input1)
+{
+    print(i)
+}
